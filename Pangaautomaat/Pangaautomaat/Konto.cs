9d7788a6c835cf2a73342fbc1b4465576296a60f
JaanJah <jaanmarkus.jahilo@tptlive.ee>
2017-12-05 09:48:19 +0200

    alustasin uuesti

--- Pangaautomaat/Pangaautomaat/Konto.cs
+++ Pangaautomaat/Pangaautomaat/Konto.cs
@@ -6,8 +6,56 @@ using System.Threading.Tasks;
 
 namespace Pangaautomaat
 {
-    class Konto
+    static class Konto
     {
-        static string DirectoryPath = "kontoinfod";
+        public static string kasutajanimi { get; set; }
+        public static int kasutajapin { get; set;}
+        public static int mitmeskasutaja { get; set; }
+
+        public static void Registreerimine()
+        {
+            ProoviUuesti:
+            Console.WriteLine("Soovisite registreerida!\nSisestage kasutajanimi (Peab olema 6 tähte): ");
+            kasutajanimi = Console.ReadLine();
+            if (kasutajanimi.Length != 6)
+            {
+                Console.WriteLine("Te ei sisestanud 6 tähelist kasutajanime. Proovige uuesti!");
+                goto ProoviUuesti;
+            }
+            Console.WriteLine("Sisestage endale sobiv PIN (4 numbrit): ");
+            if (kasutajapin > 9999 && kasutajapin.ToString().Length < 3)
+            {
+                Console.WriteLine("Te ei sisestanud 4 kohalist PINi. Proovige uuesti!");
+                goto ProoviUuesti;
+            }
+            kasutajapin = int.Parse(Console.ReadLine());
+
+            System.IO.File.AppendAllText("../../kontolist.txt", kasutajanimi + " " + kasutajapin + Environment.NewLine);
+            return;
+        }
+
+        public static void Logimine()
+        {
+            mitmeskasutaja = 0;
+
+            Console.WriteLine("Soovisite sisselogida!\nSisestage kasutajanimi: ");
+            kasutajanimi = Console.ReadLine();
+            Console.WriteLine("Sisestage oma PIN (4 numbrit)");
+            kasutajapin = int.Parse(Console.ReadLine());
+
+            using (System.IO.StreamReader sr = new System.IO.StreamReader("../../kontolist.txt"))
+            {
+                string rida;
+
+                if ((rida = sr.ReadLine()) != null)
+                {
+                    mitmeskasutaja++;
+
+
+                }
+
+            }
+        }
+
     }
 }
--- Pangaautomaat/Pangaautomaat/Program.cs
+++ Pangaautomaat/Pangaautomaat/Program.cs
@@ -6,71 +6,11 @@ using System.Threading.Tasks;
 
 namespace Pangaautomaat
 {
-    class Program : Konto
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Tere tulemast pangaautomaati, kas soovite sisselogida või regada?");
-            string valik1 = Console.ReadLine();
-            Sisselogimine:
-            if (valik1 == "sisselogida")
-            {
-                using (System.IO.StreamReader sr = new System.IO.StreamReader("../../kontoinfod.txt"))
-                Console.WriteLine("Sisestage kasutajanimi (ainult tähed): ");
-                Konto.kasutajanimi = Console.ReadLine();
-                //kui kasutaja ühildub sellega mis ennem salvestatud
-                Console.WriteLine("Sisestage oma 4-kohaline PIN: ");
-                int kasutajapin = int.Parse(Console.ReadLine());
-                //kui kasutajapin ühildub sellega mis ennem salvestatud
-                Console.WriteLine("Teie kontol on: " + kontojääk +"eur(i)");
-                Console.WriteLine("Kas soovite raha sisestada või väljastada?");
-                string valik2 = Console.ReadLine();
-                if (valik2 == "sisestada")
-                {
-                    Console.WriteLine("Kui palju sooviksite panna kontole?");
-                    int rahasisse = int.Parse(Console.ReadLine());
-                    kontojääk = kontojääk + rahasisse;
-                    Console.WriteLine("Raha on edukalt kasutajale pandud, head päeva!");
-                    Console.ReadLine();
-                }
-                else if (valik2 == "väljastada")
-                {
-                    Väljastada:
-                    Console.WriteLine("Kui suure summa sooviksite väljastada?");
-                    int rahavälja = int.Parse(Console.ReadLine());
-                    if (rahavälja > kontojääk)
-                    {
-                        Console.WriteLine("Nii suurt summat pole võimalik välja võtta, palun proovige uuesti.");
-                        goto Väljastada;
-                    }
-                    else if (rahavälja < kontojääk)
-                    {
-                        kontojääk = kontojääk - rahavälja;
-                        Console.WriteLine("Raha on edukalt välja võetud, head päeva!");
-                        Console.ReadLine();
-                    }
-                }
-
-            }
-            else if (valik1 == "regada")
-            {
-                Konto konto = new Konto();
-                Console.WriteLine("Valige endale kasutajanimi (ainult tähed): ");
-                konto.kasutajanimi = Console.ReadLine();
-                Console.WriteLine("Looge endale 4-kohaline PIN: ");
-                konto.kasutajapin = int.Parse(Console.ReadLine());
-                Console.WriteLine("Kasutaja on loodud, kas soovite sisselogida? (jah/ei)");
-                string valik4 = Console.ReadLine();
-                System.IO.File.AppendAllText("../../accounts.txt", konto.kasutajanimi + " " + konto.kasutajapin + Environment.NewLine);
-                if (valik4 == "jah")
-                {
-                    goto Sisselogimine;
-                }
-                else if (valik4 == "ei")
-                {
-                    Console.ReadLine();
-                }
-            }
+            Console.WriteLine("Tere olen pangaautomaat!\nKas soovite: (Valige number)\n1. Logida sisse.\n2. Luua konto.\n");  
         }
     }
 }
